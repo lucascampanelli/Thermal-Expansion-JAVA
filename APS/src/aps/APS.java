9cd187a6d130f8ae6851667bee2af46c85468e7e
Guilherme <guilherme.rodriguesdasilva1010@gmail.com>
2020-04-01 16:52:30 -0300

    Create the method Math

--- APS/src/aps/APS.java
+++ APS/src/aps/APS.java
@@ -5,6 +5,8 @@
  */
 package aps;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Gui
@@ -16,13 +18,29 @@ public class APS {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner selector = new Scanner(System.in);
+        int operation = 0;
+        do{
+            System.out.println("*****Calculadora de dilatação de sólidos*****");
+            System.out.println("\nDigite o número referente a opção desejada");
+            System.out.println("1 - Mostrar todos os materiais já cadastrados");
+            System.out.println("2 - Incluir material");
+            System.out.println("3 - Excluir material");
+            System.out.println("4 - ");
+            System.out.println("5 - ");
+            System.out.println("6 - ");
+            System.out.println("0 - Sair");
+        }while(operation != 0);
         
-        //Apenas para teste dos metodos da classe Materials
-        Materials rm = new Materials();        
-        String material = rm.delete("chumbo");
-        System.out.println(material);
-        rm.show();
-        //---------------------------
+        
+        
+
+    }
+    
+    public static void clearScreen(){
+        for(int i = 0;i < 15;i++){
+            System.out.println("");
+        }
     }
     
 }
---
+++ APS/src/aps/Math.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aps;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+/**
+ *
+ * @author Gui
+ */
+public class Math {
+    //Recebe um vetor com 4 valores, o primeiro comprimento inicial, coeficiente de 
+    //dilatação linear e a variação de temperatura 
+    //retorna um vetor com dois valores, um é a variação e a outra é o comprimento final
+    public String[] linearDilation(double[] values){
+        double dilation = values[0] * values[1] * values[2];
+        
+        NumberFormat format = new DecimalFormat("0.########");
+        
+        String results[] = new String[2];
+        
+        results[0] = format.format(dilation);
+        results[1] = format.format(values[0] + dilation);
+        
+        return results;
+    } 
+            
+    public String[] superficialDilation(double[] values){
+        double dilation = values[0] * (values[1] * 2) * values[2];
+        
+        NumberFormat format = new DecimalFormat("0.########");
+        
+        String results[] = new String[2];
+        
+        results[0] = format.format(dilation);
+        results[1] = format.format(values[0] + dilation);
+        
+        return results;
+    }
+    
+    public String[] volumetricDilation(double[] values){
+        double dilation = values[0] * (values[1] * 3) * values[2];
+        
+        NumberFormat format = new DecimalFormat("0.########");
+        
+        String results[] = new String[2];
+        
+        results[0] = format.format(dilation);
+        results[1] = format.format(values[0] + dilation);
+        
+        return results;
+    }
+}
